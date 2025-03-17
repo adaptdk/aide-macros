242fff3099c342a87e12bcdded69d9c7fb3b09df
Andreas Skovdal <andreasskovdal@gmail.com>
2025-03-17 09:55:52 +0100

    Add new utils and improve `aide_docs` macro (#1)

--- aide-axum-utils/src/lib.rs
+++ aide-axum-utils/src/lib.rs
@@ -2,15 +2,19 @@ use aide::{
     axum::{routing::ApiMethodRouter, ApiRouter},
     generate::GenContext,
     openapi::{
-        HeaderStyle::Simple, Operation, Parameter, ParameterData, ParameterSchemaOrContent::Schema,
-        Response, SchemaObject,
+        CookieStyle::Form, HeaderStyle::Simple, MediaType, Operation, Parameter, ParameterData,
+        ParameterSchemaOrContent::Schema, Response as OpenApiResponse, SchemaObject,
     },
     transform::TransformOperation,
     OperationOutput,
 };
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{
+    body::Bytes,
+    http::{HeaderMap, StatusCode},
+    response::{IntoResponse, Response as AxumResponse},
+};
 
-/// Wraps an API router which adds a tag to all routes
+/// Wraps an API router to add tags to all its routes
 pub struct TagApiRouter<S> {
     inner: ApiRouter<S>,
     tag: &'static str,
@@ -30,6 +34,11 @@ impl<S: Clone + Send + Sync + 'static> TagApiRouter<S> {
             .api_route_with(path, method_router, |item| item.tag(self.tag));
         self
     }
+
+    pub fn nest(mut self, path: &str, router: ApiRouter<S>) -> Self {
+        self.inner = self.inner.nest(path, router);
+        self
+    }
 }
 
 impl<S> From<TagApiRouter<S>> for ApiRouter<S> {
@@ -46,11 +55,11 @@ pub fn route_info(
     move |op| op.summary(summary).description(description)
 }
 
-/// Returns 204 No Content in Axum routes
+/// Responds with a `204 No Content` status code when returned from Axum routes.
 pub struct NoContent;
 
 impl IntoResponse for NoContent {
-    fn into_response(self) -> axum::response::Response {
+    fn into_response(self) -> AxumResponse {
         (StatusCode::NO_CONTENT, ()).into_response()
     }
 }
@@ -61,7 +70,7 @@ impl OperationOutput for NoContent {
     fn inferred_responses(
         ctx: &mut GenContext,
         operation: &mut Operation,
-    ) -> Vec<(Option<u16>, Response)> {
+    ) -> Vec<(Option<u16>, OpenApiResponse)> {
         vec![(
             Some(StatusCode::NO_CONTENT.as_u16()),
             <() as OperationOutput>::operation_response(ctx, operation).unwrap(),
@@ -69,33 +78,146 @@ impl OperationOutput for NoContent {
     }
 }
 
-pub fn simple_header(name: String, description: String, ctx: &mut GenContext) -> Parameter {
-    let s = ctx.schema.subschema_for::<String>();
+/// Returns a header parameter to be used with [aide::operation::add_parameters].
+pub fn simple_header(
+    name: String,
+    description: String,
+    required: bool,
+    ctx: &mut GenContext,
+) -> Parameter {
     Parameter::Header {
-        parameter_data: ParameterData {
-            name,
-            description: Some(description),
-            required: true,
-            deprecated: Default::default(),
-            format: Schema(SchemaObject {
-                json_schema: s,
-                example: Default::default(),
-                external_docs: Default::default(),
-            }),
-            example: Default::default(),
-            examples: Default::default(),
-            explode: Default::default(),
-            extensions: Default::default(),
-        },
+        parameter_data: simple_parameter_data(name, description, required, ctx),
         style: Simple,
     }
 }
 
+/// Returns a cookie parameter to be used with [aide::operation::add_parameters].
+pub fn simple_cookie(
+    name: String,
+    description: String,
+    required: bool,
+    ctx: &mut GenContext,
+) -> Parameter {
+    Parameter::Cookie {
+        parameter_data: simple_parameter_data(name, description, required, ctx),
+        style: Form,
+    }
+}
+
+/// Returns parameter data to be used with [aide::openapi::parameter::Parameter]
+pub fn simple_parameter_data(
+    name: String,
+    description: String,
+    required: bool,
+    ctx: &mut GenContext,
+) -> ParameterData {
+    let s = ctx.schema.subschema_for::<String>();
+    ParameterData {
+        name,
+        required,
+        description: Some(description),
+        deprecated: Default::default(),
+        format: Schema(SchemaObject {
+            json_schema: s,
+            example: Default::default(),
+            external_docs: Default::default(),
+        }),
+        example: Default::default(),
+        examples: Default::default(),
+        explode: Default::default(),
+        extensions: Default::default(),
+    }
+}
+
+/// When a route uses the proc macro `#[aide_docs]`, calling this macro with the router's name
+/// expands to an ApiMethodRouter to be used in [aide::axum::ApiRouter::api_route].
 #[macro_export]
 macro_rules! with_aide_docs {
     ($method:ident, $handler:ident) => {
         paste::paste! {
-            aide::axum::routing::[< $method _with>]($handler, [<aide_docs_ $handler>]())
+            aide::axum::routing::[< $method _with>]($handler, [<__aide_docs_ $handler>]())
         }
     };
+}
+
+/// To be returned in Axum routes, for routes that returns headers.
+pub struct WithHeaderMap<T: IntoResponse + OperationOutput>(pub HeaderMap, pub T);
+
+impl<T: IntoResponse + OperationOutput> IntoResponse for WithHeaderMap<T> {
+    fn into_response(self) -> AxumResponse {
+        (self.0, self.1).into_response()
+    }
+}
+
+impl<T: IntoResponse + OperationOutput> OperationOutput for WithHeaderMap<T> {
+    type Inner = T;
+
+    fn inferred_responses(
+        ctx: &mut GenContext,
+        operation: &mut Operation,
+    ) -> Vec<(Option<u16>, OpenApiResponse)> {
+        T::inferred_responses(ctx, operation)
+    }
+}
+
+/// Responds with the specified status code when returned from Axum routes.
+pub struct WithStatusCode<T: IntoResponse + OperationOutput>(pub StatusCode, pub T);
+
+impl<T: IntoResponse + OperationOutput> IntoResponse for WithStatusCode<T> {
+    fn into_response(self) -> AxumResponse {
+        (self.0, self.1).into_response()
+    }
+}
+
+impl<T: IntoResponse + OperationOutput> OperationOutput for WithStatusCode<T> {
+    type Inner = T;
+
+    fn inferred_responses(
+        ctx: &mut GenContext,
+        operation: &mut Operation,
+    ) -> Vec<(Option<u16>, OpenApiResponse)> {
+        T::inferred_responses(ctx, operation)
+    }
+}
+
+/// Specifies "text/plain" content type in the documentation when returned from Axum routes.
+pub struct TextPlain<T: IntoResponse + OperationOutput = Bytes>(pub T);
+
+impl<T: IntoResponse + OperationOutput> IntoResponse for TextPlain<T> {
+    fn into_response(self) -> AxumResponse {
+        self.0.into_response()
+    }
+}
+
+impl<T: IntoResponse + OperationOutput> OperationOutput for TextPlain<T> {
+    type Inner = T;
+
+    fn operation_response(
+        ctx: &mut GenContext,
+        _operation: &mut Operation,
+    ) -> Option<OpenApiResponse> {
+        let s = ctx.schema.subschema_for::<String>();
+        Some(OpenApiResponse {
+            description: "plain text".into(),
+            content: From::from([(
+                "text/plain".into(),
+                MediaType {
+                    schema: Some(SchemaObject {
+                        json_schema: s,
+                        example: Default::default(),
+                        external_docs: Default::default(),
+                    }),
+                    ..Default::default()
+                },
+            )]),
+            ..Default::default()
+        })
+    }
+
+    fn inferred_responses(
+        ctx: &mut GenContext,
+        operation: &mut Operation,
+    ) -> Vec<(Option<u16>, OpenApiResponse)> {
+        Vec::from([(Some(200), Self::operation_response(ctx, operation).unwrap())])
+    }
 }
